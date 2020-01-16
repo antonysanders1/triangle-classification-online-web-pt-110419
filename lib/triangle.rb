0eb66042bd09af86df4e6e2ebc5207554a8dc512
Antony Sanders <antony.devon92@gmail.com>
2020-01-16 01:42:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,28 @@
-require 'pry'
 class Triangle
-  # write code here
-  
-  def initialize(side_1,side_2,side_3)
-    #binding.pry
+
+  def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end 
-  
-  def kind 
-    #binding.pry
-      if @side_1 == @side_2 && @side_3
-        :equilateral
-      elsif @side_1 - 1 && (@side_2 == @side_3)
-        :isosceles
-      elsif @side_2 - 1 && (@side_1 == @side_3)
-        :isosceles
-      elsif @side_3 - 1 && (@side_1 == @side_2)
-        :isosceles
-      else :scalene
-      end
   end
-  
-    class TriangleError < StandardError
-    # triangle error code
+
+  def kind
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+      elsif
+       (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
     end
-  
-  
-  
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
