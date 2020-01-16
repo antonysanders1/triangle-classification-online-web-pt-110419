1737eef5e0ae70abc23f467878e2f0228d8b81fa
Antony Sanders <antony.devon92@gmail.com>
2020-01-16 01:42:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
   # write code here
+  
+  def initialize(side_1,side_2,side_3)
+    #binding.pry
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end 
+  
+  def kind 
+    #binding.pry
+      if @side_1 == @side_2 && @side_3
+        :equilateral
+      elsif @side_1 - 1 && (@side_2 == @side_3)
+        :isosceles
+      elsif @side_2 - 1 && (@side_1 == @side_3)
+        :isosceles
+      elsif @side_3 - 1 && (@side_1 == @side_2)
+        :isosceles
+      else :scalene
+      end
+  end
+  
+    class TriangleError < StandardError
+    # triangle error code
+    end
+  
+  
+  
 end
